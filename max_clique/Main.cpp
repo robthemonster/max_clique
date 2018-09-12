648c866e408a825f42bb4417a10a7d893365f63a
robthemonster <robthemonsterlevin@gmail.com>
2018-09-12 17:01:44 -0400

    Modified algorithm to match MWCP from paper.

--- max_clique/Main.cpp
+++ max_clique/Main.cpp
@@ -7,24 +7,36 @@
 using namespace std;
 
 bool **graph; //2d adj matrix
+int * weights; //weighted of nodes
 int * localBest; //best solution in local search space
 int localBestSize = 0;
+long localBestWeight = 0;
 int * bestSolution; //best solution so far
 int bestSolutionSize = 0;
+long bestSolutionWeight = 0;
 int *solution; //current working solution
 int solutionSize = 0;
+long solutionWeight = 0;
 bool * solutionContains; //map indicating whether a given vertex is contained in solution
 int * addList; //list of vertices to add to solution
+int * addDeltas;
 int addListSize = 0;
 int * swapList; // list of vertices to swap into solution
+int * swapDeltas;
 int * swapOutList; //correspecitng list of vertices to swap out
 int * swapOutIndexList; //index of the element to be swapped out of solution
 int swapListSize = 0;
+int greatestDelta = INT8_MIN;
+int * tiedDeltas;
+int  * tiedDeltasNeighborhoods; //tiedDeltasNeighborhoods[i] \in {0,1,2} correspond to add/swap/delete
+int tiedDeltasSize = 0; 
 int * tabuList; //list of tabu vertices
 int * tabuMap; //corresponding map to the vertex number of a given tabu list entry
 bool * tabuContains; //map indicating whether a given vertex is contained in the tabu list
 int tabuListSize = 0;
 int numVertices;
+
+const int ADD = 0, SWAP = 1, REMOVE = 2;
 
 random_device rd;
 mt19937 mersenneTwister(rd());
@@ -34,6 +46,7 @@ void runHeuristic(long long, long long);
 void deallocateGraph();
 void setInitialSolution();
 void setAddAndSwapLists();
+void selectBestDelta();
 void addRandom();
 void swapRandom();
 void removeRandom();
@@ -41,15 +54,21 @@ void decrementTabu();
 string isBestAClique();
 
 int main() {
-	parseGraphFromFile("p_hat500-1.clq");
-	runHeuristic(4000, 10000);
+	parseGraphFromFile("p_hat1500-3.clq");
+	runHeuristic(4000, 100000000);
 	cout << "Best solution found: " << bestSolutionSize << " isClique: " << isBestAClique() << endl;
+	long weight = 0;
 	cout << "[";
 	for (int i = 0; i < bestSolutionSize; i++) {
 		cout << bestSolution[i] << " ";
+		weight += weights[bestSolution[i]];
 	}
 	cout << "]" << endl;
+	cout << "Weight: " << weight << endl;
+	
 	deallocateGraph();
+	char c;
+	cin >> c;
 }
 
 string isBestAClique() {
@@ -73,35 +92,29 @@ void runHeuristic(long long maxUnimproved, long long maxIterations) {
 		long long unimprovedCtr = 0;
 		while (unimprovedCtr < maxUnimproved) {
 			setAddAndSwapLists();
-			if (addListSize > 0) {
-				addRandom();
-			}
-			else {
-				if (swapListSize > 0) {
-					swapRandom();
-				}
-				else if (solutionSize > 0) {
-					removeRandom();
-				}
-			}
+			
+			selectBestDelta();
+
 			unimprovedCtr++;
 			iterationCtr++;
 			decrementTabu();
-			if (solutionSize > localBestSize) {
+			if (solutionWeight > localBestWeight) {
 				for (int i = 0; i < solutionSize; i++) {
 					localBest[i] = solution[i];
 				}
 				localBestSize = solutionSize;
+				localBestWeight = solutionWeight;
 				unimprovedCtr = 0;
 			}
 		}
-		if (localBestSize > bestSolutionSize) {
+		if (localBestWeight > bestSolutionWeight) {
 			for (int i = 0; i < localBestSize; i++) {
 				bestSolution[i] = localBest[i];
 			}
 			bestSolutionSize = localBestSize;
+			bestSolutionWeight = localBestWeight;
 			auto diff = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - startTime).count();
-			cout << "Iteration:" << iterationCtr << " Clique Size:" << bestSolutionSize << " Timer:" << diff << "ms" << endl;
+			cout << "Iteration:" << iterationCtr << " Clique Size:" << bestSolutionSize << " Clique Weight: " << bestSolutionWeight << " Timer:" << diff << "ms" << endl;
 		}
 	}
 }
@@ -153,6 +166,43 @@ void removeRandom() {
 	solutionSize--;
 }
 
+void selectBestDelta()
+{
+	uniform_int_distribution<int> dist(0, tiedDeltasSize - 1);
+	int randomDeltaIndex = dist(mersenneTwister);
+	int listIndex = tiedDeltas[randomDeltaIndex];
+	if (tiedDeltasNeighborhoods[randomDeltaIndex] == ADD) {
+		solution[solutionSize] = addList[listIndex];
+		solutionSize++;
+		solutionWeight += weights[solution[solutionSize - 1]];
+		solutionContains[solution[solutionSize - 1]] = true;
+	}
+	else if (tiedDeltasNeighborhoods[randomDeltaIndex] == SWAP) {
+		solution[swapOutIndexList[listIndex]] = swapList[listIndex];
+		uniform_int_distribution<int> tabuTimer(1, swapListSize);
+		int timer = tabuTimer(mersenneTwister);
+		tabuList[tabuListSize] = timer + 7;
+		tabuMap[tabuListSize] = swapOutList[listIndex];
+		tabuListSize++;
+		tabuContains[swapOutList[listIndex]] = true;
+		solutionContains[swapList[listIndex]] = true;
+		solutionContains[swapOutList[listIndex]] = false;
+		solutionWeight += weights[swapList[listIndex]] - weights[swapOutList[listIndex]];
+	}
+	else {
+		tabuList[tabuListSize] = 7;
+		tabuMap[tabuListSize] = solution[listIndex];
+		tabuListSize++;
+		tabuContains[solution[listIndex]] = true;
+		solutionContains[solution[listIndex]] = false;
+		solutionWeight -= weights[solution[listIndex]];
+		swap(solution[listIndex], solution[solutionSize - 1]);
+		solutionSize--;
+	}
+	tiedDeltasSize = 0;
+	greatestDelta = INT8_MIN;
+}
+
 void setAddAndSwapLists() {
 	addListSize = 0, swapListSize = 0;
 	for (int i = 1; i <= numVertices; i++) {
@@ -162,6 +212,18 @@ void setAddAndSwapLists() {
 		int swapOutIndex = -1;
 		if (!tabuContains[i] && !solutionContains[i]) {
 			for (int j = 0; j < solutionSize; j++) {
+				int removeDelta = weights[solution[j]] * -1;
+				if (removeDelta > greatestDelta) {
+					greatestDelta = solution[j] * -1;
+					tiedDeltas[0] = j;
+					tiedDeltasNeighborhoods[0] = REMOVE;
+					tiedDeltasSize = 1;
+				}
+				else if (removeDelta == greatestDelta) {
+					tiedDeltas[tiedDeltasSize] = j;
+					tiedDeltasNeighborhoods[tiedDeltasSize] = REMOVE;
+					tiedDeltasSize++;
+				}
 				if (graph[i][solution[j]]) {
 					connected++;
 				}
@@ -177,12 +239,36 @@ void setAddAndSwapLists() {
 			if (connected == solutionSize) {
 				addList[addListSize] = i;
 				addListSize++;
+				int addDelta = weights[i];
+				if (addDelta > greatestDelta) {
+					greatestDelta = addDelta;
+					tiedDeltas[0] = addListSize - 1;
+					tiedDeltasNeighborhoods[0] = ADD;
+					tiedDeltasSize = 1;
+				}
+				else if (addDelta == greatestDelta) {
+					tiedDeltas[tiedDeltasSize] = addListSize - 1;
+					tiedDeltasNeighborhoods[tiedDeltasSize] = ADD;
+					tiedDeltasSize++;
+				}
 			}
 			else if (connected == solutionSize - 1) {
 				swapList[swapListSize] = i;
 				swapOutList[swapListSize] = swapOut;
 				swapOutIndexList[swapListSize] = swapOutIndex;
 				swapListSize++;
+				int swapDelta = weights[i] - weights[swapOut];
+				if (swapDelta > greatestDelta) {
+					greatestDelta = swapDelta;
+					tiedDeltas[0] = swapListSize - 1;
+					tiedDeltasNeighborhoods[0] = SWAP;
+					tiedDeltasSize = 1;
+				}
+				else if (swapDelta == greatestDelta) {
+					tiedDeltas[tiedDeltasSize] = swapListSize - 1;
+					tiedDeltasNeighborhoods[tiedDeltasSize] = SWAP;
+					tiedDeltasSize++;
+				}
 			} 
 		}
 	}
@@ -195,6 +281,7 @@ void setInitialSolution() {
 	int randomVertex = dist(mersenneTwister);
 	solution[0] = randomVertex;
 	solutionSize = 1;
+	solutionWeight = weights[solution[0]];
 	solutionContains[randomVertex] = true;
 	bool foundMore = false;
 	do {
@@ -211,6 +298,7 @@ void setInitialSolution() {
 				solution[solutionSize] = i;
 				solutionContains[i] = true;
 				solutionSize++;
+				solutionWeight += weights[i];
 				foundMore = true;
 			}
 		}
@@ -232,7 +320,9 @@ void parseGraphFromFile(string filename) {
 					if (ctr == 3) {
 						numVertices = stoi(curr);
 						graph = new bool*[numVertices + 1];
+						weights = new int[numVertices + 1];
 						for (int i = 0; i <= numVertices; i++) {
+							weights[i] = (i % 200) + 1;
 							graph[i] = new bool[numVertices + 1]{ false };
 						}
 						solution = new int[numVertices];
@@ -241,6 +331,8 @@ void parseGraphFromFile(string filename) {
 						swapList = new int[numVertices];
 						swapOutList = new int[numVertices];
 						swapOutIndexList = new int[numVertices];
+						tiedDeltas = new int[numVertices];
+						tiedDeltasNeighborhoods = new int[numVertices];
 						tabuList = new int[numVertices];
 						tabuMap = new int[numVertices];
 						tabuContains = new bool[numVertices + 1];
@@ -273,18 +365,21 @@ void parseGraphFromFile(string filename) {
 
 void deallocateGraph() {
 	for (int i = 0; i < numVertices + 1; i++) {
-		delete graph[i];
+		delete[] graph[i];
 	}
-	delete graph;
-	delete solution;
-	delete solutionContains;
-	delete localBest;
-	delete bestSolution;
-	delete addList;
-	delete swapList;
-	delete swapOutList;
-	delete swapOutIndexList;
-	delete tabuList;
-	delete tabuMap;
-	delete tabuContains;
+	delete[] graph;
+	delete[] weights;
+	delete[] solution;
+	delete[] solutionContains;
+	delete[] localBest;
+	delete[] bestSolution;
+	delete[] addList;
+	delete[] swapList;
+	delete[] swapOutList;
+	delete[] swapOutIndexList;
+	delete[] tiedDeltas;
+	delete[] tiedDeltasNeighborhoods;
+	delete[] tabuList;
+	delete[] tabuMap;
+	delete[] tabuContains;
 }
